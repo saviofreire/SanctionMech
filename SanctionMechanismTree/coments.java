281e8878c516df87801b18d4290807ae90ab0928
saviofreire <savio.essf@gmail.com>
2015-07-07 16:27:16 -0300

    Update coments.java

--- SanctionMechanismTree/coments.java
+++ SanctionMechanismTree/coments.java
@@ -1,41 +1,48 @@
 
-(i)		Incluir um atributo que trate sobre as metas que est„o no operador ESCOLHA. 
+(i)		Incluir um atributo que trate sobre as metas que est√£o no operador ESCOLHA. 
 		Lembre-se que o mecanismo deve perceber apenas a meta que foi realmente escolhida;
 
 	/*	Eu fui olhar no MOISE+ e ele trata as metas em escolha da seguinte forma: se duas metas A e B estao em escolha,
-	 * 	isso n„o significa necessariamente que uma delas deve ser escolhida previamente, mas que
-	 * 	qualquer uma delas pode ser executada. PorÈm uma e somente uma deve ser executada.
-	 * 	Ent„o no algoritmo eu verifico 
-	 * 	se A e B n„o foram cumpridas ambas recebem puniÁ„o individual
-	 * 	se A nao foi cumprida e B sim ninguem recebe puniÁ„o
-	 * 	se B n„o foi cumprida e A sim ninguem recebe piniÁ„o
-	 * 
+	 * 	isso n√£o significa necessariamente que uma delas deve ser escolhida previamente, mas que
+	 * 	qualquer uma delas pode ser executada. Por√©m uma e somente uma deve ser executada.
+	 * 	Ent√£o no algoritmo eu verifico 
+	 * 	se A e B n√£o foram cumpridas ambas recebem puni√ß√£o individual
+	 * 	se A nao foi cumprida e B sim ninguem recebe puni√ß√£o
+	 * 	se B n√£o foi cumprida e A sim ninguem recebe pini√ß√£o
+	 *
+	 *      SAVIO: Ent√£o deve ter um tempo para que o agente monitor possa verificar se alguma dessas a√ß√µes foram executadas,
+	 *      n√©? SUGEST√ÉO: Eu colocaria um atributo para indicar qual a meta que foi escolhida.
 	 */
 
 
-(ii) 	Percebi que vocÍ implementou a escolha das metas que ser„o cumpridas ou n„o utilizando Threads. 
-		PoderÌamos deixar mais genÈrica para que facilitasse a execuÁ„o dos prÛximos estudos de caso;
+(ii) 	Percebi que voc√™ implementou a escolha das metas que ser√£o cumpridas ou n√£o utilizando Threads. 
+		Poder√≠amos deixar mais gen√©rica para que facilitasse a execu√ß√£o dos pr√≥ximos estudos de caso;
 
-	/*	Na verdade a funÁ„o execuÁ„o de metas foi criada pra exemplificar 
-	 * 	foi onde eu defini que o g12 iria falhar porÈm esta execuÁ„o acontecer· a medida que os agentes 
-	 * 	autonomicamente realizam suas tarefas. AÌ entra o c·lculo ddos cutos.
-	 * 
+	/*	Na verdade a fun√ß√£o execu√ß√£o de metas foi criada pra exemplificar 
+	 * 	foi onde eu defini que o g12 iria falhar por√©m esta execu√ß√£o acontecer√° a medida que os agentes 
+	 * 	autonomicamente realizam suas tarefas. A√≠ entra o c√°lculo ddos cutos.
+	 *
+	 *      SAVIO: Isso mesmo! Mas voc√™ poderia deixar esse m√©todo gen√©rico para que consigamos rodar outros estudos de 
+	 *      caso com facilidade, colocando par√¢metros de entrada.
 	 */
-(iii) Comparei o algoritmo com o artigo que enviamos para o CLEI e n„o foi incluÌda a entidade Contrato;
-
+(iii) Comparei o algoritmo com o artigo que enviamos para o CLEI e n√£o foi inclu√≠da a entidade Contrato;
+         /*     SAVIO: o contrato seria um conjunto de a√ß√µes, conforme conversamos pelo celular... Temos que ter a aprova√ß√£o
+         *      dos professores.
+         */
 
 (iv) 	Analisando os resultados, seria interessante colocar o valor do custo coletivo e individual separados. 
 		Veja os resultados do mesmo estudo de caso que calculamos no artigo:
 	
-	/*	O resultado g1	Cvmf = -3 (aqui seria apenas o custo gerencial, n„o?)
-	 *  que deu diferente È porque o cÛdigo somava a individual + a gerencial da meta g1
-	 *  pq alÈm de ser uma meta gerencial ela tambÈm est· na hierarquia e se violada recebe individual
+	/*	O resultado g1	Cvmf = -3 (aqui seria apenas o custo gerencial, n√£o?)
+	 *  que deu diferente √© porque o c√≥digo somava a individual + a gerencial da meta g1
+	 *  pq al√©m de ser uma meta gerencial ela tamb√©m est√° na hierarquia e se violada recebe individual
 	 *  pois ela faz parte da sequencia g1 g2 g3
-	 *  o cÛdigo agora calcula separos os custos
+	 *  o c√≥digo agora calcula separos os custos
 	 *  
 	 *  
-	 *  A arquitetura È construÌda pela ·rvore
-	 *  e a contagem de sanÁıes È feita pela "leitura" da ·rvore,
+	 *  A arquitetura √© constru√≠da pela √°rvore
+	 *  e a contagem de san√ß√µes √© feita pela "leitura" da √°rvore,
 	 *  ou seja leitura da hierarquia em si.
 	 *  
+	 *   SAVIO:OK!
 	 */
